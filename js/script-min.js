3df59f4ce54d5e84681c877950901488154dd73d
Ryan Struhl <keisans@me.com>
2012-05-09 11:01:24 -0400

    Get rid of some extra code
    Signed-off-by: Ryan Struhl <keisans@me.com>

--- js/script-min.js
+++
@@ -1,4 +0,0 @@
-//File: Script.js
-//Site specific javascript goes here
-//Demo Code/////////////////////////
-$(function(){var a=function(){var b=$(".block_test div");b.each(function(a){var b=$(this),c=b.width()+"px";b.attr("title",c)})};a();$(window).resize(function(){a()})});
